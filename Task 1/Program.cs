674c97496726d2b87bfd4ec760a2dbd5de84d776
Burundukov Denis <burundukov.d@yandex.ru>
2022-08-11 18:08:44 +0500

    Task 1 finished

---
+++ Task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b > a) 
+{
+    Console.WriteLine("Второе число больше");
+}
+else if(a > b)
+{
+    Console.WriteLine("Первое число больше");
+}
+else
+{
+    Console.WriteLine("Оба числа равны");
+}
+
