add0f25f3b4f4500e30011d84029360e7680e192
Burundukov Denis <burundukov.d@yandex.ru>
2022-08-11 18:32:21 +0500

    Task 2 completed

---
+++ Task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье целое число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+if (b > a && c < a)
+{
+    Console.WriteLine($"{b} - максимальное число");
+}
+else if (a > c && a > b)
+{
+    Console.WriteLine($"{a} - максимальное число");
+}
+else
+{
+    Console.WriteLine($"{c} - максимальное число");
+}
