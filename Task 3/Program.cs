2a9616c1d0abe618693837b3b59eb35e68511e9e
Burundukov Denis <burundukov.d@yandex.ru>
2022-08-11 18:59:48 +0500

    Task 3 completed

---
+++ Task 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число, чтобы проверить делится ли оно на 2");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Число делится на 2 без остатка");
+}
+else
+{
+    Console.WriteLine("Число делится на 2 с остатком");
+}
