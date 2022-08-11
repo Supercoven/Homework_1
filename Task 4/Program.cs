e22bf627ec33c33273b6e9b7a265ebd4964e89f9
Burundukov Denis <burundukov.d@yandex.ru>
2022-08-11 19:24:48 +0500

    task 4 completed

---
+++ Task 4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 0; i <= n; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine($"{i}");
+    }
+}
